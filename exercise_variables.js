89fd0386ea09a4c0916b6ed50bfecf045bd19b29
socent <brad_wilcox@me.com>
2013-06-17 18:16:54 -0400

    first commit: problem 1

---
+++ exercise_variables.js
@@ -0,0 +1,14 @@
+// THE FORTUNE TELLER
+// Why pay a fortune teller when you can just program your fortune yourself?
+
+// Store the following into variables: number of children, partner's name, geographic location, job title.
+// Output your fortune to the screen like so: "You will be a X in Y, and married to Z with N kids."
+
+var numKids = 2;
+var partner = 'Monika';
+var location = 'Toronto';
+var jobTitle = 'entrepreneur';
+
+var future = 'You will be a ' + jobTitle + ' in ' + location + ', and married to ' + 
+	partner + ' with ' + numKids + ' kids.';
+console.log(future);
