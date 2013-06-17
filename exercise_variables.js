3278b88bb9225d40e7c014581fddee2e5ea4bd2d
socent <brad_wilcox@me.com>
2013-06-17 18:22:55 -0400

    second problem completed.

--- exercise_variables.js
+++ exercise_variables.js
@@ -12,3 +12,17 @@ var jobTitle = 'entrepreneur';
 var future = 'You will be a ' + jobTitle + ' in ' + location + ', and married to ' + 
 	partner + ' with ' + numKids + ' kids.';
 console.log(future);
+
+// THE AGE CALCULATOR
+// Forgot how old you are? Calculate it!
+
+// Store the current year in a variable.
+// Store your birth year in a variable.
+// Calculate your 2 possible ages based on the stored values.
+// Output them to the screen like so: "You are either NN or NN", substituting the values.
+
+var currentYear = 2013;
+var birthYear = 1979;
+
+var age = currentYear - birthYear
+console.log('You are either ' + age + ' or ' + (age - 1));
